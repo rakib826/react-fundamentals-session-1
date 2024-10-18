8a12a8e20677eed609c240b72cbdac22189be78f
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-18 22:07:08 +0600

    jsx file export import

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Products from './components/Products/Products'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h2>Vite + React</h2>
+      <Products></Products>
     </>
   )
 }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <div>
+      <h3>I am from Products</h3>
+    </div>
+  )
+}
