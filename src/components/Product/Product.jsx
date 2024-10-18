0034fd19dd743caf4bb5128032268e4faaa20cd3
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-19 00:23:00 +0600

    hu

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Product({info}) {
   console.log(info)
   return (
-    <div>This is a product</div>
+    <div>This is a product {info.message} {info.phone}</div>
   )
 }
